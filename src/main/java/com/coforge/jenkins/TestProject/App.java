c2e2996aa90386ef39c27ef42dcb761980d835c0
monikat13lncte <monikat1399@gmail.com>
2020-12-21 14:11:47 +0530

    Test project commit

---
+++ src/main/java/com/coforge/jenkins/TestProject/App.java
@@ -0,0 +1,13 @@
+package com.coforge.jenkins.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
